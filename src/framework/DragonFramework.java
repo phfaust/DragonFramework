6595297f279513be3b4e479d456f0a0c544c9ee2
eddrichang <eddrichang@gmail.com>
2017-04-01 22:36:19 +0800

    hint logic for room2

--- src/framework/DragonFramework.java
+++ src/framework/DragonFramework.java
@@ -72,22 +72,26 @@ public class DragonFramework {
 	    		case "hint":
 	    			//System.out.println("Yay hint but this command is not implemented yet. \n");
 	    			//hint logic here;
-	    			System.out.println("Here are your list of commands:");
+	    			System.out.println("You are in: " + currentRoom);
+	    			System.out.println("Hint:");
 	    			switch(currentRoom){
 	    				case "Room2":
-	    					System.out.println("You can choose to 'swim' or 'look'");
-	    					System.out.println(currentRoom);
+//	    					when all tasks have been done by that room
+//	    					System.out.println("gatestate" + gameState);
+	    					if(gameState == 15){
+	    						System.out.println("You might want to move rooms now.");
+	    					} else {
+	    						System.out.println("You can choose to 'swim' or 'look'");
+	    					}
 	    					break;
 	    				case "Room3":
 	    					System.out.println("You can choose to 'attack' or 'look'");
-	    					System.out.println(currentRoom);
 	    					break;
 	    				case "Room4":
 	    					System.out.println("You can choose to 'answer' or 'look'");
-	    					System.out.println(currentRoom);
 	    					break;
 	    				case "Room5":
-//	    					System.out.println("You can choose to 'answer' or 'look'");
+	    					
 	    					System.out.println(currentRoom);
 	    					break;
 	    			}    			
