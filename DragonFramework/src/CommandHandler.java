4b3a797445d817d9ab60f4ee554360ed34aa2244
eddrichang <eddrichang@gmail.com>
2017-04-01 15:37:40 +0800

    deleted old version

--- DragonFramework/src/CommandHandler.java
+++
@@ -1,7 +0,0 @@
-import room.*;
-import java.lang.reflect.Method;
-
-public interface CommandHandler 
-{
-	public void process(Object o, String command, Method m, DragonFramework df) throws Exception;
-}
