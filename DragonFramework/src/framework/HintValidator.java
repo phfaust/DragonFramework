8c8950ef3a70cd264e41e7e04cd0212140c84596
irene <theabermejo@gmail.com>
2017-04-19 11:46:41 +0800

    Added classes yay

---
+++ DragonFramework/src/framework/HintValidator.java
@@ -0,0 +1,14 @@
+package framework;
+
+import java.lang.reflect.Method;
+
+@CommandAnnotation(target=Command.class)
+public class HintValidator implements CommandHandler {
+
+	@Override
+	public void process(Object o, Object arg, Method m, DragonFramework df) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
