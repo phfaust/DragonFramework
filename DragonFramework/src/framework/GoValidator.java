009cb752cda79e67e1ec6468492b05ef9028ac29
irene <theabermejo@gmail.com>
2017-04-30 21:06:40 +0800

    Add ReadStrategy

--- DragonFramework/src/framework/GoValidator.java
+++ DragonFramework/src/framework/GoValidator.java
@@ -1,5 +1,17 @@
 package framework;
 
-public class GoValidator {
+import java.lang.reflect.Method;
+
+@CommandAnnotation(target=Command.class)
+public class GoValidator implements CommandHandler {
+
+	@Override
+	public void process(Object o, Object arg, Method m, DragonFramework df) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
+
+
+
--- DragonFramework/src/framework/RegisterValidator.java
+++ DragonFramework/src/framework/RegisterValidator.java
@@ -1,5 +1,14 @@
 package framework;
 
-public class RegisterValidator {
+import java.lang.reflect.Method;
+
+@CommandAnnotation(target=Command.class)
+public class RegisterValidator implements CommandHandler {
+
+	@Override
+	public void process(Object o, Object arg, Method m, DragonFramework df) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
